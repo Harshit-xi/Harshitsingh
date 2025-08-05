25f5b9cce1ca0fe1016c895db1f8da4b7fc14f5a
Harshit-xi <harshitxi9@gmail.com>
2025-08-05 20:22:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 
+const roles = ["Full-stack Developer", "UI/UX Enthusiast", "Creative Coder"];
+let roleIndex = 0;
+let charIndex = 0;
+const typingElement = document.getElementById("typing");
+
+function type() {
+  if (charIndex < roles[roleIndex].length) {
+    typingElement.textContent += roles[roleIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, 100);
+  } else {
+    setTimeout(erase, 2000);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    typingElement.textContent = roles[roleIndex].substring(0, charIndex - 1);
+    charIndex--;
+    setTimeout(erase, 50);
+  } else {
+    roleIndex = (roleIndex + 1) % roles.length;
+    setTimeout(type, 500);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  setTimeout(type, 500);
+});
+
